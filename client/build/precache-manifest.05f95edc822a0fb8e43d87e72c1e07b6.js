e33fd7efa8f01732ef6c20b28e8cb1834eb604d5
Abhishek Khanna <abhishek.khanna@teamlance.io>
2020-02-18 13:16:34 +0530

    build added

---
+++ client/build/precache-manifest.05f95edc822a0fb8e43d87e72c1e07b6.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b4ab6f89fe8154484652691e3680a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11ba80ef816ab04180d3",
+    "url": "./static/css/2.613c2d51.chunk.css"
+  },
+  {
+    "revision": "ea32931294617f0f5e88",
+    "url": "./static/css/main.fe82c1ae.chunk.css"
+  },
+  {
+    "revision": "11ba80ef816ab04180d3",
+    "url": "./static/js/2.11ee25e0.chunk.js"
+  },
+  {
+    "revision": "ea32931294617f0f5e88",
+    "url": "./static/js/main.2a7b7fc8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c43388ff88282b27a8b3cedad451d536",
+    "url": "./static/media/anil-godhani.c43388ff.png"
+  },
+  {
+    "revision": "3d5fa04cfbdfde348733ae88729838ce",
+    "url": "./static/media/bhavin-patoliya.3d5fa04c.png"
+  },
+  {
+    "revision": "540b49fdc4aef8ba1feb3cfc170bb56e",
+    "url": "./static/media/chirag-bhut.540b49fd.png"
+  },
+  {
+    "revision": "bcfea50a7ccc78894a42844e3919be84",
+    "url": "./static/media/dhaval-kathiriya.bcfea50a.png"
+  },
+  {
+    "revision": "a436a471ac2a2b4bb4b1802ea91f2e9b",
+    "url": "./static/media/director.a436a471.png"
+  },
+  {
+    "revision": "e3acfa9406a0de053e13eebc19c12e7b",
+    "url": "./static/media/harshank-khunt.e3acfa94.png"
+  },
+  {
+    "revision": "57a5fb2eb0312c94aade92a417a311a9",
+    "url": "./static/media/hepin-rabadiya.57a5fb2e.png"
+  },
+  {
+    "revision": "33318f825490fff3f4904ccd7b7d5eb3",
+    "url": "./static/media/hiren-patel.33318f82.png"
+  },
+  {
+    "revision": "c36071cd5b135167f9498d58075af609",
+    "url": "./static/media/ic_banner_top.c36071cd.png"
+  },
+  {
+    "revision": "0de3c78a0e96fd60e45809d5b07bbb98",
+    "url": "./static/media/instagram.0de3c78a.png"
+  },
+  {
+    "revision": "5986257402d92f2db4dd931f1a7f94ac",
+    "url": "./static/media/jayesh-ahir.59862574.png"
+  },
+  {
+    "revision": "94ab7221fb714d87499c38f568168bd5",
+    "url": "./static/media/jignesh-patel.94ab7221.png"
+  },
+  {
+    "revision": "2f195339870994e5b8d8cea3d428f048",
+    "url": "./static/media/keval-patel .2f195339.png"
+  },
+  {
+    "revision": "6b884f6903c20bb8763de3e1f616d671",
+    "url": "./static/media/kishan-hirpara.6b884f69.png"
+  },
+  {
+    "revision": "fb6d90168a4f90eea177f71e6e1eb7c0",
+    "url": "./static/media/legal-1.fb6d9016.png"
+  },
+  {
+    "revision": "ba9fc3b0c35a68a8e2e52cbcfcc4d6ae",
+    "url": "./static/media/legal-2.ba9fc3b0.png"
+  },
+  {
+    "revision": "fffcf512ca40b7409fe4b9e8ee4a189b",
+    "url": "./static/media/legal-3.fffcf512.png"
+  },
+  {
+    "revision": "7bab6b0f58e155b3f884f93b18144d36",
+    "url": "./static/media/legal-4.7bab6b0f.jpg"
+  },
+  {
+    "revision": "91d741eb0a0342f92007b04f24aa1325",
+    "url": "./static/media/logo.91d741eb.png"
+  },
+  {
+    "revision": "e211e55187552cd9580d8975a0514f6c",
+    "url": "./static/media/mayur-gondliya.e211e551.png"
+  },
+  {
+    "revision": "43a063d603bcae0d3aaf61d77cad2d64",
+    "url": "./static/media/milan-padhara.43a063d6.png"
+  },
+  {
+    "revision": "28ef180b71990dcdc1a399d7a5b69a2a",
+    "url": "./static/media/nitin-ahir.28ef180b.png"
+  },
+  {
+    "revision": "a80f42e217710f9e6f21c5f9c46c3144",
+    "url": "./static/media/noImage.a80f42e2.jpg"
+  },
+  {
+    "revision": "a82317b899feaa8a1641d2c7c7305227",
+    "url": "./static/media/rahul-dethariya.a82317b8.png"
+  },
+  {
+    "revision": "667565a764b8df47648d9b85bd4fa06c",
+    "url": "./static/media/rahul-rathod.667565a7.png"
+  },
+  {
+    "revision": "b6c1a32597a4656ad659345d65e7e2a8",
+    "url": "./static/media/satish-boghara.b6c1a325.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f62fe9e2db4ab54e5bad953ef77e0c36.js"
+  "./precache-manifest.05f95edc822a0fb8e43d87e72c1e07b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
